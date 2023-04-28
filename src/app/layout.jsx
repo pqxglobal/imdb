e9cc8daf37b3b4168d9e33db7d2d1c2f738121ab
pqxglobal <l.r.slegt@gmail.com>
2023-04-28 15:15:01 +0200

    ✨ create the header section

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,5 @@
+import Header from "@/components/Header";
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "IMDb Clone",
@@ -15,7 +13,17 @@ export default function RootLayout({ children }) {
   return (
     <>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body>
+          {/* Header */}
+          <Header />
+
+          {/* Navbar */}
+
+          {/* SearchBox */}
+          
+          {children}
+          
+          </body>
       </html>
     </>
   );
