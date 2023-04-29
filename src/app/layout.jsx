f941f37992abd5f331b905a3330902ce00ec0f5d
pqxglobal <l.r.slegt@gmail.com>
2023-04-29 21:51:51 +0200

    ✨ add search ability

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import "./globals.css";
 import Providers from "./Providers";
 import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
 
 export const metadata = {
   title: "IMDb Clone",
@@ -25,6 +26,8 @@ export default function RootLayout({ children }) {
             <Navbar />
 
             {/* SearchBox */}
+
+            <SearchBox />
 
             {children}
           </Providers>
